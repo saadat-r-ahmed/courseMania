da4637694e0a99d00d3ac6babf064b08b79488ae
Saadat Rafid Ahmed <63824872+saadat-r-ahmed@users.noreply.github.com>
2021-12-24 22:20:53 +0600

    [1.2] Basic Styling added to all the pages, template created, some more functions created in main.py
    Styling added with bootstrap
    Template created with navigation bar
    All the variable caught using flask

--- main.py
+++ main.py
@@ -22,12 +22,59 @@ def teacher_login():
 def admin_login():
     return render_template('admin_login.html')
 
+
 # ! ALL PAGE LOGIN AUTHENTICATOR
+@app.route('/admin_authinticate', methods = ['GET', 'POST'])
+def admin_authinticate():
+    if request.method == "POST":
+        adminID = request.form["adminid"]
+        password = request.form["password"]
+        typ = str(type(adminID))
+        return render_template('show_message.html', message = adminID+' '+password)
+    
+    if request.method == "GET":
+        if 'adminID' in request.args:
+            adminID = request.args["adminid"]
+            password = request.args["password"]
+            return render_template('show_message.html', message = adminID+' '+password)
+
+@app.route('/student_authinticate', methods = ['GET', 'POST'])
+def student_authinticate():
+    if request.method == "POST":
+        studentID= request.form["studentid"]
+        password = request.form["password"]
+        return render_template('show_message.html', message = 'Studnet getting ready')
+    
+    if request.method == "GET":
+        if 'studentid' in request.args:
+            studentID = request.args["studentid"]
+            password = request.args["password"]
+            return render_template('show_message.html', message = 'Studnet getting ready')        
+
+
+
+@app.route('/teacher_authinticate', methods = ['GET', 'POST'])
+def teacher_authinticate():
+    if request.method == "POST":
+        teacherID= request.form["teacherid"]
+        password = request.form["password"]
+        return render_template('show_message.html', message = 'Teacher getting ready')
+    
+    if request.method == "GET":
+        if 'teacherid' in request.args:
+            teacherID = request.args["teacherid"]
+            password = request.args["password"]
+            return render_template('show_message.html', message = 'Teacher getting ready')
+    
+
+        
+# ! FORGOTTEN PASSWORD
+@app.route('/forgotten_password')
+def forgotten_password():
+    return render_template('forgotten_password.html')
 
 
 # ! FLASH ERROR IN A NEW PAGE
-
-
 
 
 
